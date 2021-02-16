830f81ddcf2e7505ac0e11acf8c59dc38407b7e9
Jingjian Wei <44480478+wjj31767@users.noreply.github.com>
2021-02-16 23:00:45 +0100

    add plot for O2 and CH4

--- evaluate.py
+++ evaluate.py
@@ -21,12 +21,14 @@ net.eval()
 data = THERMO('data/0.022000625/',False)
 train_iter = Data.DataLoader(data, 400, shuffle=False)
 input = []
+oinput = []
 output = []
 ori_output = []
 train_l_sum = 0.0
 loss =nn.MSELoss()
 for X,y in tqdm(train_iter):
-    input.append(X)
+    input.append(X[:,4])
+    oinput.append(X[:,15])
     y_hat = net(X)
     output.append(y_hat[:,4])
     ori_output.append(y[:,4])
@@ -37,8 +39,21 @@ print("loss sum: ",train_l_sum)
 input = torch.cat(input)
 output = torch.cat(output)
 ori_output = torch.cat(ori_output)
+oinput = torch.cat(oinput)
 print(ori_output)
 # print(input.shape,output.shape,ori_output.shape)
+
+im = oinput.detach().numpy()
+plt.hist(im)
+plt.show()
+print(im.min(),im.max())
+
+im = input.detach().numpy()
+plt.hist(im)
+plt.show()
+print(im.min(),im.max())
+
+
 im = output.detach().numpy()
 
 # mean = data.summean[:,4]
@@ -58,7 +73,7 @@ im = (output-ori_output).detach().numpy()
 # mean = data.summean[:,4]
 # std = data.sumstd[:,4]
 # im = im*std+mean
-plt.hist(im,805)
+plt.hist(abs(im),800)
 plt.show()
 print(im.min(),im.max(),abs(im).min())
 
